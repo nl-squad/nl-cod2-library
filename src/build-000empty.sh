54274814009effe98bef3879366abd960c579854
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2023-05-04 10:00:15 +0200

    Save SHA of 000empty.iwd

--- src/build-000empty.sh
+++ src/build-000empty.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+cwd="$(dirname "$(realpath "$0")")"
+echo "Executing in $cwd"
+
+new_sha=$(find $cwd/iwds -type f -exec sha256sum {} \; | sort -k 2 | sha256sum)
+old_sha=""
+if [ -f $cwd/iwds_sum ]; then
+    old_sha=$(cat $cwd/iwds_sum)
+fi
+
+echo "new_sha=$new_sha"
+echo "old_sha=$old_sha"
+
+if [[ "$new_sha" == "$old_sha" ]]; then
+    echo "It seems that the iwds didn't change"
+    echo "Skipping 000empty.iwd build"
+    exit 0
+fi
 
 temp_dir=./tmp-000empty
 mkdir -p "${temp_dir}/soundaliases"
@@ -13,13 +30,13 @@ name,sequence,file,vol_min,vol_max,vol_mod,pitch_min,pitch_max,dist_min,dist_max
 nl_empty_${timestamp},,null.wav,1,1,,,,,,music,loaded,,nonlooping,,,,,,,,,,
 EOF
 
-for file in src/iwds/*.iwds; do
+for file in iwds/*.iwds; do
     filename=$(basename "$file" .iwd)
     echo " - maps/mp/${filename}.csv"
     touch "${temp_dir}/maps/mp/${filename}.csv"
 done
 
-rm ~/cod2/library/000empty.iwd
-(cd $temp_dir && zip -q -r ~/cod2/library/000empty.iwd .)
-chmod 744 ~/cod2/library/000empty.iwd # make this readable for reverse-proxy
+rm -f $cwd/000empty.iwd
+(cd $temp_dir && zip -q -r $cwd/000empty.iwd .)
+chmod 744 $cwd/000empty.iwd # make this readable for reverse-proxy
 rm -r "${temp_dir}"
